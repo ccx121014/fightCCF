1f222925d33b5cfd35ac36dc4dd0c856e7604e55
ccx <ccx1014ccx@outlook.com>
2025-12-20 15:25:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,19 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 
+// UTF-8编码转换函数
+function utf8_to_b64(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode(parseInt(p1, 16));
+    }));
+}
+
+function b64_to_utf8(str) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
 // 账号系统
 function createAccount(username) {
     console.log('Creating account for:', username);
